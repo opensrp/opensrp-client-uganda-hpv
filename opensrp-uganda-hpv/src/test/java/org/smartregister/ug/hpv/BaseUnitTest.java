68252910bdbdbb14a281095e05d6650ebcd25037
Martin Ndegwa <ndegwamartin@gmail.com>
2018-03-12 12:35:09 +0300

    Basic Project set up with build , CI and Test Configuration

---
+++ opensrp-uganda-hpv/src/test/java/org/smartregister/ug/hpv/BaseUnitTest.java
@@ -0,0 +1,21 @@
+package org.smartregister.ug.hpv;
+
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+/**
+ * Created by ndegwamartin on 12/03/2018.
+ */
+
+
+@RunWith(PowerMockRunner.class)
+@PowerMockRunnerDelegate(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
+@PowerMockIgnore({"org.mockito.*", "org.robolectric.*", "android.*"})
+public abstract class BaseUnitTest {
+
+}
