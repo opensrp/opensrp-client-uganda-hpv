921d7733d3c669cfa3b827776d885d6cb1f1a4fe
Vincent Karuri <vincentmkaruri@gmail.com>
2018-03-19 17:01:36 +0300

    Add preferences.xml definitions

---
+++ opensrp-uganda-hpv/src/main/java/org/smartregister/ug/hpv/application/HpvApplication.java
@@ -0,0 +1,8 @@
+package org.smartregister.ug.hpv.application;
+
+/**
+ * Created by vkaruri on 19/03/2018.
+ */
+
+public class HpvApplication {
+}
---
+++ opensrp-uganda-hpv/src/main/java/util/UgandaHpvConstants.java
@@ -0,0 +1,8 @@
+package util;
+
+/**
+ * Created by vkaruri on 19/03/2018.
+ */
+
+public class UgandaHpvConstants {
+}
