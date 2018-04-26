f2cbe549cea6b3d8a0663a2704fa61775c41fdb3
Vincent Karuri <vincentmkaruri@gmail.com>
2018-04-26 10:58:24 +0300

    Refactor and add unit tests

--- opensrp-uganda-hpv/src/test/java/org/smartregister/ug/hpv/LoginActivityTest.java
+++ opensrp-uganda-hpv/src/test/java/org/smartregister/ug/hpv/LoginActivityTest.java
@@ -2,10 +2,7 @@ package org.smartregister.ug.hpv;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -15,23 +12,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.powermock.reflect.Whitebox;
 import org.robolectric.Robolectric;
-import org.robolectric.RuntimeEnvironment;
-import org.robolectric.Shadows;
 import org.robolectric.android.controller.ActivityController;
-import org.robolectric.shadows.ShadowActivity;
 import org.robolectric.shadows.ShadowAlertDialog;
 import org.robolectric.shadows.ShadowApplication;
-import org.robolectric.shadows.ShadowConnectivityManager;
-import org.robolectric.shadows.ShadowNetworkInfo;
 import org.smartregister.ug.hpv.activity.HomeRegisterActivity;
 import org.smartregister.ug.hpv.activity.LoginActivity;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.*;
-import static org.robolectric.Shadows.shadowOf;
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 
 /**
  * Created by vkaruri on 24/04/2018.
@@ -41,172 +30,110 @@ public class LoginActivityTest extends BaseUnitTest {
 
     private LoginActivity loginActivity = new LoginActivity();
     private ActivityController<LoginActivity> controller;
-    private ConnectivityManager connectivityManager;
-    private ShadowNetworkInfo shadowOfActiveNetworkInfo;
-    private ShadowConnectivityManager shadowConnectivityManager;
 
     @Before
     public void setUp() {
         controller = Robolectric.buildActivity(LoginActivity.class).create().start();
         loginActivity = controller.get();
-        connectivityManager = (ConnectivityManager) RuntimeEnvironment.application.getSystemService(Context.CONNECTIVITY_SERVICE);
     }
 
     @Test
     public void testUserNameEditTextIsInitialized() {
 
-        Field userNameEditTextField = getReflectedField(loginActivity, "userNameEditText");
-        EditText userNameEditText = null;
-        try {
-            userNameEditText = (EditText) userNameEditTextField.get(loginActivity);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-
+        EditText userNameEditText = Whitebox.getInternalState(loginActivity, "userNameEditText");
         assertNotNull(userNameEditText);
     }
 
     @Test
     public void testPasswordEditTextIsInitialized() {
 
-        Field userPasswordEditTextField = getReflectedField(loginActivity, "passwordEditText");
-        EditText userPasswordEditText = null;
-        try {
-            userPasswordEditText = (EditText) userPasswordEditTextField.get(loginActivity);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-
+        EditText userPasswordEditText = Whitebox.getInternalState(loginActivity, "passwordEditText");
         assertNotNull(userPasswordEditText);
     }
 
 
     @Test
     public void testShowPasswordCheckBoxIsInitialized() {
-        Field showPasswordCheckBoxField = getReflectedField(loginActivity, "showPasswordCheckBox");
-        CheckBox showPasswordCheckBox = null;
 
-        try {
-            showPasswordCheckBox = (CheckBox) showPasswordCheckBoxField.get(loginActivity);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-
+        CheckBox showPasswordCheckBox = Whitebox.getInternalState(loginActivity, "showPasswordCheckBox");
         assertNotNull(showPasswordCheckBox);
     }
 
     @Test
     public void testProgressDialogIsInitialized() {
-        Field progressDialogField = getReflectedField(loginActivity, "progressDialog");
-        ProgressDialog progressDialog = null;
 
-        try {
-            progressDialog = (ProgressDialog) progressDialogField.get(loginActivity);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-
+        ProgressDialog progressDialog = Whitebox.getInternalState(loginActivity, "progressDialog");
         assertNotNull(progressDialog);
     }
 
     @Test
-    public void testInitializeBuildDetails() {
+    public void testBuildDetailsAreInitialized() {
+
         TextView buildDetails = (TextView) loginActivity.findViewById(R.id.login_build_text_view);
-        assertNotEquals(buildDetails.getText(), null);
-    }
-
-
-    private void testRemoteLogin_validCredentials_shouldLogin() {
-
-        loginActivity = Robolectric.setupActivity(LoginActivity.class);
-        Button loginButton = (Button) loginActivity.findViewById(R.id.login_login_btn);
-
-        Field userNameEditTextField = getReflectedField(loginActivity, "userNameEditText");
-        EditText userNameEditText = null;
-        try {
-            userNameEditText = (EditText) userNameEditTextField.get(loginActivity);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-
-
-        Field userPasswordEditTextField = getReflectedField(loginActivity, "passwordEditText");
-        EditText userPasswordEditText = null;
-        try {
-            userPasswordEditText = (EditText) userPasswordEditTextField.get(loginActivity);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-
-        userNameEditText.setText("demo");
-        userPasswordEditText.setText("Amani123");
-
-        loginButton.performClick();
-
-        assertActivityStarted(loginActivity);
+        assertNotNull(buildDetails.getText());
     }
 
     @Test
-    public void testUnauthorizedUserDialogIsDisplayed() {
-        loginActivity = Robolectric.setupActivity(LoginActivity.class);
-        Button loginButton = (Button) loginActivity.findViewById(R.id.login_login_btn);
+    public void testUnauthorizedUserDialogIsDisplayed_missingCredentials() {
 
-        Field userNameEditTextField = getReflectedField(loginActivity, "userNameEditText");
-        EditText userNameEditText = null;
-        try {
-            userNameEditText = (EditText) userNameEditTextField.get(loginActivity);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
+        attemptInvalidLogin("", "");
+        assertNotNull(ShadowAlertDialog.getLatestAlertDialog());
+    }
 
+    @Test
+    public void testUnauthorizedUserDialogIsDisplayed_missingUserName() {
 
-        Field userPasswordEditTextField = getReflectedField(loginActivity, "passwordEditText");
-        EditText userPasswordEditText = null;
-        try {
-            userPasswordEditText = (EditText) userPasswordEditTextField.get(loginActivity);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
+        attemptInvalidLogin("", "password");
+        assertNotNull(ShadowAlertDialog.getLatestAlertDialog());
+    }
 
-        userNameEditText.setText("");
-        userPasswordEditText.setText("");
+    @Test
+    public void testUnauthorizedUserDialogIsDisplayed_missingPassword() {
 
-        try {
-            Whitebox.invokeMethod(loginActivity, LoginActivity.class, "login", loginButton, false);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        attemptInvalidLogin("username", "");
+        assertNotNull(ShadowAlertDialog.getLatestAlertDialog());
+    }
 
+    @Test
+    public void testUnauthorizedUserDialogIsDisplayed_invalidCredentials() {
+
+        attemptInvalidLogin("username", "password");
         assertNotNull(ShadowAlertDialog.getLatestAlertDialog());
     }
 
     @Test
     public void testGoToHome() {
+
         try {
             Whitebox.invokeMethod(loginActivity, LoginActivity.class, "goToHome", false);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        assertActivityStarted(loginActivity);
+        assertActivityStarted(loginActivity, new HomeRegisterActivity());
     }
 
-    private void assertActivityStarted(Activity activity) {
+    private void assertActivityStarted(Activity currActivity, Activity nextActivity) {
 
-        Intent expectedIntent = new Intent(activity, HomeRegisterActivity.class);
+        Intent expectedIntent = new Intent(currActivity, nextActivity.getClass());
         Intent actual = ShadowApplication.getInstance().getNextStartedActivity();
         assertEquals(expectedIntent.getComponent(), actual.getComponent());
     }
 
-    public Field getReflectedField(Activity activity, String fieldName) {
-        Field field = null;
-        try{
-            field = activity.getClass().getDeclaredField(fieldName);
-        } catch (NoSuchFieldException e) {
+    private void attemptInvalidLogin(String userName, String password) {
+
+        loginActivity = Robolectric.setupActivity(LoginActivity.class);
+        Button loginButton = (Button) loginActivity.findViewById(R.id.login_login_btn);
+
+        EditText userNameEditText = Whitebox.getInternalState(loginActivity, "userNameEditText");
+        EditText userPasswordEditText = Whitebox.getInternalState(loginActivity, "passwordEditText");
+
+        userNameEditText.setText(userName);
+        userPasswordEditText.setText(password);
+        try {
+            Whitebox.invokeMethod(loginActivity, LoginActivity.class, "login", loginButton, false);
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
-        field.setAccessible(true);
-        return field;
     }
 }
